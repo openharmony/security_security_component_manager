8985ed9b8737045cbde7b19182d805854abde2e2
AXYChen <chenmingyang14@huawei.com>
2025-04-19 18:38:58 +0800

    fuzz覆盖率
    Signed-off-by: AXYChen <chenmingyang14@huawei.com>
    Change-Id: Ia8a4c9588ff8f84db5c0da604ad09252f569aa65

--- test/fuzztest/security_component/service/unregistersecuritycomponentstub_fuzzer/unregistersecuritycomponentstub_fuzzer.cpp
+++ test/fuzztest/security_component/service/unregistersecuritycomponentstub_fuzzer/unregistersecuritycomponentstub_fuzzer.cpp
@@ -22,7 +22,9 @@
 #include "isec_comp_service.h"
 #include "sec_comp_enhance_adapter.h"
 #include "sec_comp_info.h"
+#define private public
 #include "sec_comp_service.h"
+#undef private
 #include "securec.h"
 #include "token_setproc.h"
 #include "unregistersecuritycomponentstub_fuzzer.h"
@@ -30,56 +32,6 @@
 using namespace OHOS::Security::SecurityComponent;
 using namespace OHOS::Security::AccessToken;
 namespace OHOS {
-static int32_t RegisterSecurityComponentStub(uint32_t type, const std::string& compoInfo)
-{
-    uint32_t code =
-        static_cast<uint32_t>(ISecCompServiceIpcCode::COMMAND_REGISTER_SECURITY_COMPONENT);
-    MessageParcel rawData;
-    MessageParcel input;
-    SecCompRawdata inputData;
-    MessageParcel reply;
-    SecCompRawdata replyData;
-
-    if (!input.WriteInterfaceToken(ISecCompService::GetDescriptor())) {
-        return 0;
-    }
-    if (!rawData.WriteUint32(type)) {
-        return 0;
-    }
-
-    if (!rawData.WriteString(compoInfo)) {
-        return 0;
-    }
-    SecCompEnhanceAdapter::EnhanceClientSerialize(rawData, inputData);
-    input.WriteUint32(inputData.size);
-    input.WriteRawData(inputData.data, inputData.size);
-    MessageOption option(MessageOption::TF_SYNC);
-    auto service =
-        std::make_shared<SecCompService>(SA_ID_SECURITY_COMPONENT_SERVICE, true);
-    service->OnRemoteRequest(code, input, reply, option);
-    if (!reply.ReadUint32(replyData.size)) {
-        return 0;
-    }
-    auto readRawReply = reply.ReadRawData(replyData.size);
-    if (readRawReply == nullptr) {
-        return 0;
-    }
-    int32_t res = replyData.RawDataCpy(readRawReply);
-    if (res != SC_OK) {
-        return 0;
-    }
-    MessageParcel deserializedReply;
-    SecCompEnhanceAdapter::EnhanceClientDeserialize(replyData, deserializedReply);
-    if (!deserializedReply.ReadInt32(res)) {
-        return 0;
-    }
-    int32_t scId = 0;
-    if (!deserializedReply.ReadInt32(scId)) {
-        return 0;
-    }
-    return scId;
-}
-
 static void UnregisterSecurityComponentStubFuzzTest(const uint8_t *data, size_t size)
 {
     uint32_t code = static_cast<uint32_t>(ISecCompServiceIpcCode::COMMAND_UNREGISTER_SECURITY_COMPONENT);
@@ -90,7 +42,12 @@ static void UnregisterSecurityComponentStubFuzzTest(const uint8_t *data, size_t 
     CompoRandomGenerator generator(data, size);
     uint32_t type = generator.GetScType();
     std::string compoInfo = generator.GenerateRandomCompoStr(type);
-    int32_t scId = RegisterSecurityComponentStub(type, compoInfo);
+    auto service = std::make_shared<SecCompService>(SA_ID_SECURITY_COMPONENT_SERVICE, true);
+    int32_t scId;
+    int32_t res = service->RegisterSecurityComponentBody(static_cast<SecCompType>(type), compoInfo, scId);
+    if (res != SC_OK) {
+        return;
+    }
     if (!input.WriteInterfaceToken(ISecCompService::GetDescriptor())) {
         return;
     }
@@ -102,7 +59,6 @@ static void UnregisterSecurityComponentStubFuzzTest(const uint8_t *data, size_t 
     input.WriteRawData(inputData.data, inputData.size);
 
     MessageOption option(MessageOption::TF_SYNC);
-    auto service = std::make_shared<SecCompService>(SA_ID_SECURITY_COMPONENT_SERVICE, true);
     service->OnRemoteRequest(code, input, reply, option);
 }
 } // namespace OHOS
